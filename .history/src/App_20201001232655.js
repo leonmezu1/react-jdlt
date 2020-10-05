714116f6d2b6542d567f3cf70cdea414676f3e80
leonmezu1 <leo.mez@outlook.com>
2020-10-05 08:35:53 -0500

    Anadir formulario

--- .history/src/App_20201001232655.js
+++ .history/src/App_20201001232655.js
@@ -1,5 +1,4 @@
 import React, { Fragment } from 'react';
-import Formulario from './components/Formulario'
 
 function App() {
   return (
---
+++ src/components/Formulario.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default class Formulario extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <h1>Formulario</h1>
+    );
+  }
+}
